7b9662fb57d8f6fae144d2932a7d738f44d38c64
miiniii <miiniiiiii9@gmail.com>
2025-04-16 16:39:54 +0900

    디렉토 생성 및 Entity, DTO 클래스 생성

---
+++ src/main/java/com/flab/testrepojava/domain/Product.java
@@ -0,0 +1,38 @@
+package com.flab.testrepojava.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int price;
+    private int quantity;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
+
