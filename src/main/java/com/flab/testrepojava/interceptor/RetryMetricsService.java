409bba1efbdbe7cba6f29c07edc4d85f6eda6b68
miiniii <miiniiiiii9@gmail.com>
2025-06-30 22:04:55 +0900

    로그 추가

--- src/main/java/com/flab/testrepojava/interceptor/RetryMetricsService.java
+++ src/main/java/com/flab/testrepojava/interceptor/RetryMetricsService.java
@@ -1,10 +1,12 @@
 package com.flab.testrepojava.interceptor;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class RetryMetricsService {
 
     private final MeterRegistry meterRegistry;
@@ -14,6 +16,7 @@ public class RetryMetricsService {
     }
 
     public void countRetry(Exception e, Long productId) {
+        log.info("🔁 [RETRY_METRIC] 재시도 카운트 기록 - 상품 ID: {}, 예외 타입: {}", productId, e.getClass().getSimpleName());
         if (e instanceof ObjectOptimisticLockingFailureException || e instanceof IllegalStateException) {
             meterRegistry.counter("product.retry.count",
                     "exception", e.getClass().getSimpleName(),
