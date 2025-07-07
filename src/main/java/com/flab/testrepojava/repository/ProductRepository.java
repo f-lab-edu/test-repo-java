30eb90efeb8a877b8014fb9019c634a5881d2a20
miiniii <miiniiiiii9@gmail.com>
2025-07-07 13:10:59 +0900

    feat: add pessimistic and redis distributed lock logic

--- src/main/java/com/flab/testrepojava/repository/ProductRepository.java
+++ src/main/java/com/flab/testrepojava/repository/ProductRepository.java
@@ -14,7 +14,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByName(String name); //완전히 일치하는 이름만 조회
     List<Product> findByNameContaining(String name); //Like '사과%'
 
-//    @Lock(LockModeType.PESSIMISTIC_WRITE)
-//    @Query("SELECT p FROM Product p WHERE p.id = :id")
-//    Optional<Product> findByIdForUpdate(@Param("id") Long id);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    Optional<Product> findByIdForUpdate(@Param("id") Long id);
 }
